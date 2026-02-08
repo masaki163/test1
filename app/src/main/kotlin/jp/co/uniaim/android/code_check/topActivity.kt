3221d961a400fe07f3f39e5b01b49ccd8919fa94
masaki163 <masaki163@gmail.com>
2026-02-08 10:39:11 +0900

    Initial commit: add Android project

---
+++ app/src/main/kotlin/jp/co/uniaim/android/code_check/topActivity.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright © 2021 YUMEMI Inc. All rights reserved.
+ * Copyright © 2024 Uniaim co., ltd. All rights reserved.
+ */
+package jp.co.uniaim.android.code_check
+
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class TopActivity : AppCompatActivity(R.layout.activity_top) {
+
+    companion object {
+        lateinit var lastSearchDate: Date
+    }
+}
